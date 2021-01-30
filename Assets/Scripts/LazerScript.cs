e6118b2c9bcc99240781c3b96fa52df09c004fc9
Ruben-desktop <ruben-1337@hotmail.com>
2021-01-30 23:06:47 +0100

    lazer ignores steel sheep and goes through trigger colliders

--- Assets/Scripts/LazerScript.cs
+++ Assets/Scripts/LazerScript.cs
@@ -4,62 +4,69 @@ using UnityEngine;
 
 public class LazerScript : MonoBehaviour
 {
-    public AnimationCurve animationCurve;
+	public AnimationCurve animationCurve;
 
-    Transform lrTransform;
-    public LineRenderer lr;
-    Transform ps;
+	Transform lrTransform;
+	public LineRenderer lr;
+	Transform ps;
 
-    float widthFactor = 0;
-    Vector3 lrPos;
+	float widthFactor = 0;
+	Vector3 lrPos;
 
-    void Start()
-    {
-        ps = gameObject.GetComponentInChildren<ParticleSystem>().transform;
-        ps.position = new Vector3(1000, 1000, 1000);
+	void Start()
+	{
+		ps = gameObject.GetComponentInChildren<ParticleSystem>().transform;
+		ps.position = new Vector3(1000, 1000, 1000);
 
-        lrTransform = lr.gameObject.transform;
-        lr.SetPosition(0, lr.transform.position);
-        lr.widthMultiplier = widthFactor;
+		lrTransform = lr.gameObject.transform;
+		lr.SetPosition(0, lr.transform.position);
+		lr.widthMultiplier = widthFactor;
 
-        lrPos = lrTransform.TransformDirection(Vector3.forward * 1000);
-    }
+		lrPos = lrTransform.TransformDirection(Vector3.forward * 1000);
+	}
 
-    void Update()
-    {
-        this.transform.Rotate(new Vector3(0.2f,0,0));
+	void Update()
+	{
+		this.transform.Rotate(new Vector3(0.2f, 0, 0));
 
-        lr.startColor = new Color(1, widthFactor, widthFactor, widthFactor);
-        lr.endColor = new Color(1, widthFactor, widthFactor, widthFactor);
-        lr.widthMultiplier = widthFactor/4 + (Mathf.Sin(Time.time * 40) /50);
+		lr.startColor = new Color(1, widthFactor, widthFactor, widthFactor);
+		lr.endColor = new Color(1, widthFactor, widthFactor, widthFactor);
+		lr.widthMultiplier = widthFactor / 4 + (Mathf.Sin(Time.time * 40) / 50);
 
-        lr.SetPosition(0, lr.transform.position);
-        lr.SetPosition(1, lrPos);
+		lr.SetPosition(0, lr.transform.position);
+		lr.SetPosition(1, lrPos);
 
-        widthFactor = animationCurve.Evaluate(Time.time/4);
-    }
-    private void FixedUpdate()
-    {
-        RaycastHit hit;
+		widthFactor = animationCurve.Evaluate(Time.time / 4);
+	}
+	private void FixedUpdate()
+	{
+		RaycastHit hit;
 
-        if (Physics.Raycast(lr.transform.position, lr.transform.TransformDirection(Vector3.forward), out hit))
-        {
-            lrPos = hit.point;
-            if (widthFactor >= 1)
-            {
-                ps.position = hit.point;
-                ps.rotation = Quaternion.LookRotation(hit.normal);
+		if (Physics.Raycast(lr.transform.position, lr.transform.TransformDirection(Vector3.forward), out hit))
+		{
+			lrPos = hit.point;
+			if (widthFactor >= 1)
+			{
+				ps.position = hit.point;
+				ps.rotation = Quaternion.LookRotation(hit.normal);
 
-                if (hit.collider.CompareTag("Player") || hit.collider.CompareTag("Sheep"))
-                {
-                    hit.collider.GetComponent<Mammal>().Death();
-                }
-            } 
-        }
-        else
-        {
-            lrPos = lr.transform.TransformDirection(Vector3.forward * 1000);
-            ps.position = new Vector3(1000, 1000, 1000);
-        }
-    }
+				if (hit.collider.CompareTag("Player") || hit.collider.CompareTag("Sheep"))
+				{
+					if (hit.collider.gameObject.GetComponent<SteelSheep>() != null)
+					{
+						//skip
+					}
+					else
+					{
+						hit.collider.GetComponent<Mammal>().Death();
+					}	
+				}
+			}
+		}
+		else
+		{
+			lrPos = lr.transform.TransformDirection(Vector3.forward * 1000);
+			ps.position = new Vector3(1000, 1000, 1000);
+		}
+	}
 }
