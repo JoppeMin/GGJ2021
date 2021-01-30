75b06e6e4e59238db14fb76594cfc3f9cd46f957
Ruben-desktop <ruben-1337@hotmail.com>
2021-01-30 22:15:47 +0100

    interceptor sheep behaviour ready for injection into a prefab

--- Assets/Scipts/MissileInterceptor.cs
+++ Assets/Scipts/MissileInterceptor.cs
@@ -1,22 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 [RequireComponent(typeof(Collider))]//trigger collider for detecting incoming missiles
 public class MissileInterceptor : MonoBehaviour
 {
 	[SerializeField] private GameObject cannon;
+	public bool isEnabled;
 
+	private void FixedUpdate()
+	{
+		if (isEnabled)
+		{
+			cannon.transform.Rotate(0f, 10f, 0f);
+		}
+	}
 	private void OnTriggerEnter(Collider other)
 	{
-		if (other.gameObject.GetComponent<HomingMissileV2>() != null)
+		if (isEnabled)
 		{
-			if(cannon != null)
+			if (other.gameObject.GetComponent<HomingMissileV2>() != null)
 			{
-				cannon.transform.LookAt(other.transform);
-				// play PFX
+				if (cannon != null)
+				{
+					cannon.transform.LookAt(other.transform);
+					// play PFX
+				}
+				//draw laser
+				other.GetComponent<HomingMissileV2>().FuckingExplode(); //pew pew
 			}
-
-			other.GetComponent<HomingMissileV2>().FuckingExplode(); //pew pew
 		}
 	}
 }
